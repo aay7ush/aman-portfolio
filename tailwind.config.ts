db2136df7054d74364e07ae708839bc5e9d7b5e8
Aayush Jaiswal <aay7ush@gmail.com>
2023-12-04 14:59:13 +0530

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "clr-primary": "#fff",
+        "clr-secondary": "#d9d9d9",
+        "clr-background": "#151515",
+        "clr-accent": "#4ee1a0",
       },
     },
   },
